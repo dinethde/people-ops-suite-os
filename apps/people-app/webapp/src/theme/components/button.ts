36ffd567701356ef6b9689d135eaa88a15f31a2a
dinethde <dinethran33@gmail.com>
2026-02-13 12:42:55 +0530

    fix: enhance outline-neutral button variant styling and hover states

--- apps/people-app/webapp/src/theme/components/button.ts
+++ apps/people-app/webapp/src/theme/components/button.ts
@@ -74,7 +74,16 @@ export const muiButton: Components<Theme>["MuiButton"] = {
       props: { variant: "outline-neutral" as any },
       style: ({ theme }) => ({
         border: `1px solid ${theme.palette.customBorder.brand.active}`,
-        "&:hover": {},
+        color: theme.palette.customText.primary.p2.active,
+        "&:hover": {
+          borderColor: theme.palette.customBorder.primary.hover,
+          backgroundColor: theme.palette.fill.primary.light.active
+        },
+        "&.Mui-disabled": {
+          backgroundColor: "transparent",
+          borderColor: theme.palette.customBorder.brand.disabled,
+          color: theme.palette.customText.primary.p2.active,
+        }
       }),
     },
   ],
